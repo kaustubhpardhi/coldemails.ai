4a43cc6ef960a1d055f1b1d80210182212776248
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2023-01-27 22:07:12 +0530

    style component

--- src/mailai/Mailai.js
+++ src/mailai/Mailai.js
@@ -1,43 +1,47 @@
 import "./Mailai.css";
+import Style from "../style/Style";
 
 function Mailai() {
   return (
-    <div className="mail-card">
-      <div className="mail-card-left">
-        <div className="mail-details">
-          <p className="mail-details-title">Furnish your mail</p>
-          <div className="mail-details-purpose">
-            <input type="radio" value="job" name="purpose" />
-            <p className="mail-purpose-title">Job Hunting 💻</p>
-            <span className="span"></span>
-            <input type="radio" value="showcase" name="purpose" />{" "}
-            <p className="mail-purpose-title">Showcase Your Work 📜</p>
+    <div className="mail-app">
+      <div className="mail-card">
+        <div className="mail-card-left">
+          <div className="mail-details">
+            <p className="mail-details-title">Furnish your mail</p>
+            <div className="mail-details-purpose">
+              <input type="radio" value="job" name="purpose" />
+              <p className="mail-purpose-title">Job Hunting 💻</p>
+              <span className="span"></span>
+              <input type="radio" value="showcase" name="purpose" />{" "}
+              <p className="mail-purpose-title">Showcase Your Work 📜</p>
+            </div>
+            <div className="mail-recipient">
+              <p className="mail-recipient-title mail-left-title">
+                Tell us something about your recipient
+              </p>
+              <input type="text" className="mail-input" name="recipient" />
+            </div>
+            <div className="mail-sender">
+              <p className="mail-sender-title mail-left-title">
+                Tell us what you do ➡ Your Profession
+              </p>
+              <input type="text" className="mail-input" name="recipient" />
+            </div>
+            <div className="mail-provide">
+              <p className="mail-provide-title mail-left-title">
+                Tell us what you can provide to the recipient
+              </p>
+              <input type="text" className="mail-input" name="recipient" />
+            </div>
           </div>
-          <div className="mail-recipient">
-            <p className="mail-recipient-title mail-left-title">
-              Tell us something about your recipient
-            </p>
-            <input type="text" className="mail-input" name="recipient" />
-          </div>
-          <div className="mail-sender">
-            <p className="mail-sender-title mail-left-title">
-              Tell us what you do ➡ Your Profession
-            </p>
-            <input type="text" className="mail-input" name="recipient" />
-          </div>
-          <div className="mail-provide">
-            <p className="mail-provide-title mail-left-title">
-              Tell us what you can provide to the recipient
-            </p>
-            <input type="text" className="mail-input" name="recipient" />
+        </div>
+        <div className="mail-card-right">
+          <div className="mail-card-box">
+            <p className="mail-card-box-title">Your Prompts</p>
           </div>
         </div>
       </div>
-      <div className="mail-card-right">
-        <div className="mail-card-box">
-          <p className="mail-card-box-title">Your Prompts</p>
-        </div>
-      </div>
+      <Style />
     </div>
   );
 }
---
+++ src/style/Style.js
@@ -0,0 +1,22 @@
+import "./Style.css";
+
+function Style() {
+  return (
+    <div className="style-card">
+      <div className="style-card-title">
+        <p className="sttyle-card-title-text">Choose your tone 🔊</p>
+      </div>
+      <div className="style-card-list">
+        <div className="style-card-list-item">
+          <p>Formal</p>
+        </div>
+        <div className="style-card-list-item">
+          <p>Casual</p>
+        </div>
+        <div className="style-card-list-item">1</div>
+      </div>
+    </div>
+  );
+}
+
+export default Style;
