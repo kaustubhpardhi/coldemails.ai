3a6e23abd8cb541d488d306ea85d902454864f97
Kaustubh Pardhi <kaustubhpardhi006@gmail.com>
2023-02-08 19:01:12 +0530

    shipped basic version of frontend

--- src/style/Style.js
+++ src/style/Style.js
@@ -1,19 +1,27 @@
 import "./Style.css";
+import casual from "../images/casualok.png";
+import formal from "../images/formal.png";
 
 function Style() {
   return (
     <div className="style-card">
       <div className="style-card-title">
-        <p className="sttyle-card-title-text">Choose your tone 🔊</p>
+        <p className="style-card-title-text">Choose your tone 🔊</p>
       </div>
       <div className="style-card-list">
-        <div className="style-card-list-item">
-          <p>Formal</p>
+        <div className="style-card-list-item-1" tabIndex={0}>
+          <img className="casual-img" src={casual} alt="casual"></img>
+          <div className="list-item">
+            <p>Formal</p>
+          </div>
         </div>
-        <div className="style-card-list-item">
-          <p>Casual</p>
+
+        <div className="style-card-list-item-2" tabIndex={1}>
+          <img className="formal-img" src={formal} alt="formal"></img>
+          <div className="list-item-2">
+            <p> Casual</p>
+          </div>
         </div>
-        <div className="style-card-list-item">1</div>
       </div>
     </div>
   );
